585ba2d2ec19192e4bc9c6a66147ae86803a31dc
Quant14 <igrgames05@gmail.com>
2024-03-14 17:41:34 +0200

    deploy for accounts

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 from flask import Flask
+from flask_cors import CORS
 
 cred = credentials.Certificate(os.path.join(os.getcwd(), "app\\kocosi-firebase-key.json"))
 
@@ -13,6 +14,7 @@ db = firestore.client()
 auth_url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
 
 app = Flask(__name__)
+CORS(app)
 
 from app import account_routes
 from app import image_routes
