44bee924923a9c417eee1d1fd32bea3bb5206b53
Quant14 <igrgames05@gmail.com>
2024-03-14 12:55:09 +0200

    signup and login routes

--- app/views.py
+++ app/views.py
@@ -1,5 +1,30 @@
 from app import app
+from app import base
 
-@app.route('/topic', methods=['POST'])
-def get_topic():
-    return "Artidee"
+from flask import request, jsonify
+
+from firebase_admin import auth
+from firebase_admin.auth import UserRecord
+
+@app.route('/signup', methods=['POST'])
+def sign_up() -> UserRecord:
+    json_data = request.json
+    email = json_data.get('email')
+    password = json_data.get('password')
+
+    try:
+        auth.create_user(email=email, password=password)
+    except ValueError as ve:
+        # Handle ValueError (raised when email or password is invalid)
+        return jsonify({"msg": str(ve)}), 400
+    except Exception as e:
+        # Catch any other unexpected errors
+        return jsonify({"msg": str(e)}), 500
+
+    return jsonify({"msg": "User created successfully"}), 200
+
+@app.route('/login', methods=['POST'])
+def log_in() -> UserRecord:
+    json_data = request.json
+    email = json_data.get('email')
+    password = 
