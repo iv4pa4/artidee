2912e1f2401b874cb9e09b03e6971c70c620b9ad
kriss20006 <kristiyan.i.ivanov.2020@elsys-bg.org>
2024-03-15 10:41:10 +0200

    removed useless imports

--- app/prompt_route.py
+++ app/prompt_route.py
@@ -1,20 +1,6 @@
-# import json
-# import requests
-# import os
-
 from app import app
-# from app import base
-# from app import db
-# from app import auth_url
-
 from flask import request, jsonify
-
-# from firebase_admin import auth
-
-from flask import Flask, request, jsonify
-# import firebase_admin
-# from firebase_admin import credentials
-# from firebase_admin import firestore
+from flask import request, jsonify
 from app.drawing_prompt import drawing_prompt
 from app.openai_api_functions import image_generator
 
@@ -36,14 +22,3 @@ def generate_prompt():
         return jsonify({'prompt': generated_prompt, 'image_url': generated_image})
     
     return jsonify({'prompt': generated_prompt})
-
-# @app.route('/prompts/<user_id>', methods=['GET'])
-# def get_prompts(user_id):
-#     blacklist_ref = db.collection("Blacklist").document(user_id)
-#     blacklist_data = blacklist_ref.get()
-#     if not blacklist_data.exists:
-#         return jsonify({'error': 'User has no prompts'}), 404
-    
-#     prompts = blacklist_data.to_dict().get("prompts", [])
-    
-#     return jsonify({'prompts': prompts})
