d983c7eb1ec3c7fe6a087b3e2503bb616269b7e2
Quant14 <igrgames05@gmail.com>
2024-03-15 12:28:45 +0200

    friend get fixed

--- app/friend_routes.py
+++ app/friend_routes.py
@@ -14,11 +14,18 @@ def add_friend():
         if auth.get_user(uid).display_name == request.json['name']:
             query = db.collection("Connections").where('user_id_1', '==', request.json['user_id'])
             docs = query.stream()
+            query = db.collection("Connections").where('user_id_2', '==', request.json['user_id'])
+            docs2 = query.stream()
             curr_friends = []
+
             for doc in docs:
                 curr_friends.append(doc.get('user_id_2'))
+            for doc in docs2:
+                curr_friends.append(doc.get('user_id_1'))
+
             if uid in curr_friends:
                 return jsonify({"message": "Friend already in list"}), 200
+
             update_time, doc_id = db.collection("Connections").add({"user_id_1": request.json['user_id'], "user_id_2": uid})
             return jsonify({"message": "Friend added successfully"}), 200
 
@@ -32,15 +39,18 @@ def get_friends():
 
     connections_ref = db.collection("Connections")
     friends = []
+    names = []
 
     query = connections_ref.where("user_id_1", "==", user_id).get()
     for doc in query:
         friend_id = doc.to_dict()["user_id_2"]
         friends.append(friend_id)
+        names.append(auth.get_user(friend_id).display_name)
 
     query = connections_ref.where("user_id_2", "==", user_id).get()
     for doc in query:
         friend_id = doc.to_dict()["user_id_1"]
         friends.append(friend_id)
+        names.append(auth.get_user(friend_id).display_name)
 
-    return jsonify({'friends': friends})
+    return jsonify({'friends': friends, 'names': names})
